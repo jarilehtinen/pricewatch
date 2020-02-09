06a6490db3b9854cf7446a5f1a7d2d6cd94d934b
Jari Lehtinen <jari@feature.fi>
2020-02-09 15:35:25 +0200

    Added new method for displaying logged product prices

--- lib/Log.php
+++ lib/Log.php
@@ -44,4 +44,60 @@ class Log
         $data = date('Y-m-d H:i:s').'##'.$url.'##'.$price."\n";
         file_put_contents('log.txt', $data, FILE_APPEND);
     }
+
+    /**
+     * Display price log
+     */
+    public function dipslayPriceLog($product_id)
+    {
+        $this->products = new Products;
+        $this->tools = new Tools;
+
+        $products = $this->products->getProducts();
+
+        if (!$products) {
+            echo "No products found.\n";
+            exit;
+        }
+
+        if (!isset($products[$product_id-1])) {
+            echo "No product found with ID ".$product_id."\n";
+            exit;
+        }
+
+        $url = $products[$product_id-1];
+
+        $log = $this->readLog();
+        $last_price = false;
+
+        foreach ($log[$url] as $entry) {
+            if ($entry['price']) {
+                $price_increased = $last_price && $entry['price'] > $last_price;
+                $price_decreased = $last_price && $entry['price'] < $last_price;
+
+                echo date('d.m.Y H:i', strtotime($entry['date'])).' ';
+
+                if ($price_increased) {
+                    echo $this->red;
+                } elseif ($price_decreased) {
+                    echo $this->green;
+                } else {
+                    echo $this->cyan;
+                }
+
+                echo $this->tools->pricePad($entry['price']).'€';
+
+                if ($price_increased) {
+                    echo ' ▲';
+                } elseif ($price_decreased) {
+                    echo ' ▼';
+                }
+
+                echo $this->reset_color;
+                echo "\n";
+
+                $last_price = $entry['price'];
+            }
+        }
+    }
 }
